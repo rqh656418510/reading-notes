c00295b651a3ff96be7098496f09d048988053bb
rqh656418510 <656418510@qq.com>
2023-04-23 22:37:43 +0800

    feat: update configs

--- shell/deploy.sh
+++ shell/deploy.sh
@@ -9,6 +9,13 @@
 # 1. 在本地执行一键部署脚本： bash deploy.sh
 # 2. 浏览器强制刷新页面后，即可正常加载最新的 Web 资源文件，无需重启 Web 服务器
 
+# 判断当前用户是否为Root用户
+if [ ! $UID -eq 0 ]; then
+  echo -e "\033[31m Error: 请使用超级用户权限执行当前脚本 \033[0m"
+  echo -e "\033[31m Error: 程序退出执行 \033[0m"
+  exit 1
+fi
+
 # 读取INI配置文件
 function __readINI() {
  	INIFILE=$1; SECTION=$2; ITEM=$3
@@ -26,10 +33,10 @@ _PASSWORD_FILE=( $( __readINI /etc/rsyncd.ini ReadingNotes password_file ) )
 _DIST_DIR='../.vuepress/dist/'
 
 # 清理文件
-# rm -rf $_DIST_DIR
+rm -rf $_DIST_DIR
 
 # 编译构建
-# npm run docs:build
+npm run docs:build
 
 # 同步文件到生产服务器（使用 "--delete" 参数删除目标目录比源目录多余的文件）
 rsync -avzP --delete --no-o --no-g $_DIST_DIR $_USER@$_HOST::$_MODULE/ --password-file=$_PASSWORD_FILE
